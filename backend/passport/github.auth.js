f50fd9974f154c1b02edbc3189bebf81543aab7a
Aritra25 <aritra.maji2001@gmail.com>
2024-03-18 20:54:01 +0530

    updatedcallbackUrl

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -28,7 +28,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      callbackURL: "https://mern-github-app-dyhz.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       // asynchronous verification, for effect...
