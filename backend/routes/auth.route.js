1657a9c10f33508a95d54e103c9002006ce495f0
Aritra25 <aritra.maji2001@gmail.com>
2024-03-18 21:50:43 +0530

    ughfghc

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 router.get(
   "/github",
-  passport.authenticate("github", { scope: ["user.email"] })
+  passport.authenticate("github", { scope: ["user:email"] })
 );
 router.get(
   "/github/callback",
